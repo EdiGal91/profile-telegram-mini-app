17c6ad93150c0732056ca22647052875ae2528ba
EdiGal91 <47693118+EdiGal91@users.noreply.github.com>
2025-07-23 20:14:26 +0400

    save

--- src/pages/IndexPage/IndexPage.tsx
+++ src/pages/IndexPage/IndexPage.tsx
@@ -10,38 +10,10 @@ export const IndexPage: FC = () => {
   return (
     <Page back={false}>
       <List>
-        <Section
-          header="Create your profile"
-          footer="You can use these pages to learn more about features, provided by Telegram Mini Apps and other useful projects"
-        >
-          <Link to="/ton-connect">
-            <Cell
-              before={
-                <Image src={tonSvg} style={{ backgroundColor: "#007AFF" }} />
-              }
-              subtitle="Connect your TON wallet"
-            >
-              TON Connect
-            </Cell>
-          </Link>
-        </Section>
-        <Section
-          header="Application Launch Data"
-          footer="These pages help developer to learn more about current launch information"
-        >
-          <Link to="/init-data">
-            <Cell subtitle="User data, chat information, technical data">
-              Init Data
-            </Cell>
-          </Link>
-          <Link to="/launch-params">
-            <Cell subtitle="Platform identifier, Mini Apps version, etc.">
-              Launch Parameters
-            </Cell>
-          </Link>
-          <Link to="/theme-params">
-            <Cell subtitle="Telegram application palette information">
-              Theme Parameters
+        <Section header="Добро пожаловать, здесь вы можете управлять своей анкетой">
+          <Link to="/profile-create">
+            <Cell subtitle="Создать черновик  своей анкеты">
+              Создать анкету
             </Cell>
           </Link>
         </Section>
